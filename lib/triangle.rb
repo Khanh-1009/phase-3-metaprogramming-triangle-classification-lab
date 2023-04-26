3c8b5c2a4a257937b7e586c3c2041e90e7473b86
Khanh Tran <katetrannbkhanh@gmail.com>
2023-04-25 18:45:34 -0700

    commit tree b5a56fe9ffa009b1c0b19698e37b7af948c8a0ad

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
 class Triangle
   # write code here
+  attr_accessor :left, :right, :bottom
+
+  def initialize(left, right, bottom)
+    @left = left
+    @right = right
+    @bottom = bottom
+  end
+
+  def kind
+    if self.is_a?(Triangle)
+      begin
+        if self.left <= 0 && self.right <= 0 && self.bottom <= 0
+          raise TriangleError
+        elsif self.left < 0 || self.right < 0 || self.bottom < 0
+          raise TriangleError
+        elsif self.left + self.right <= self.bottom || self.left + self.bottom <= self.right || self.right + self.bottom <= self.left
+          raise TriangleError
+        end
+      end
+      if self.left == self.right && self.left == self.bottom && self.right == self.bottom
+        :equilateral
+      elsif self.left == self.right || self.left == self.bottom || self.right == self.bottom
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
